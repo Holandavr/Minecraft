5631df176032bc3719d109a686fac47bd01d9dfb
Holandavr <127700809+Holandavr@users.noreply.github.com>
2023-07-08 17:19:51 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -65,6 +65,51 @@ if (keyPressed=="39"){
     right ();
     console.log("right")
 }
+
+if(keyPressed == '87') {
+     new_image('wall.jpg');
+      console.log("w");
+     } 
+     
+     if(keyPressed == '71') { 
+        new_image('ground.png');
+         console.log("g");
+         } 
+         
+         if(keyPressed == '76') {
+             new_image('light_green.png');
+              console.log("l");
+             } 
+             
+             if(keyPressed == '84') {
+                 new_image('trunk.jpg');
+                  console.log("t");
+                 }
+
+                 if(keyPressed == '82') {
+                     new_image('roof.jpg');
+                      console.log("r"); 
+                    }
+                    
+                    if(keyPressed == '89') {
+                         new_image('yellow_wall.png');
+                          console.log("y");
+                         }
+                         
+                         if(keyPressed == '68') {
+                             new_image('dark_green.png');
+                              console.log("d");
+                             }
+                             
+                             if(keyPressed == '85') { 
+                                new_image('unique.png');
+                                 console.log("u");
+                                 } 
+                                 
+                                 if(keyPressed == '67') {
+                                     new_image('cloud.jpg');
+                                      console.log("c");
+                                     }
 }
 
 
